a18786e8bfc58e17b6719efd6c988447d36c4d74
Mr-Pine <git@mr-pine.de>
2025-11-25 11:44:43 +0100

    Remove incorrect docs on test runner

--- src/test/kotlin/BorroQTest.kt
+++ src/test/kotlin/BorroQTest.kt
@@ -4,15 +4,6 @@ import org.checkerframework.framework.test.TestRootDirectory
 import org.junit.runners.Parameterized
 import java.io.File
 
-/**
- * Test runner for tests of the Optional Demo Checker.
- *
- *
- * Tests appear as Java files in the `tests/optionaldemo` folder. To add a new test case,
- * create a Java file in that directory. The file contains "// ::" comments to indicate expected
- * errors and warnings; see
- * https://github.com/typetools/checker-framework/blob/master/checker/tests/README.
- */
 @TestRootDirectory("src/test/java/testcases")
 class BorroQTest(testFile: File?) : CheckerFrameworkPerFileTest(
     testFile,
