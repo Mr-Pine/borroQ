6ee4084afaeb34ff2d273e456db09e12a22c80cb
MisterPine <git@mr-pine.de>
2025-12-04 15:20:39 +0100

    Finish rule 1

--- src/test/java/testcases/borroq/Rules.java
+++ src/test/java/testcases/borroq/Rules.java
@@ -13,8 +13,10 @@ public interface Rules {
 
         }
 
-        @Mutable int x;
-        @Immutable int y;
+        @Mutable int m1; // TODO: Report mutable annotation on int
+        @Mutable Object m2;
+        @Immutable int i1;
+        @Immutable Object i2;
     }
 
     static void R1_a(@Mutable @Release Object x1) {
@@ -27,10 +29,24 @@ public interface Rules {
     }
 
     // :: warning: annotation.mutable.immutable.field
-    static void R1_c(@Mutable(".y") @Release A x1) {
+    static void R1_c(@Mutable(".i1") @Release A x1) {
     }
 
     // :: error: annotation.restricted.conflicting
-    static void R1_d(@Mutable({".y", ".y"}) @Release A x1) {
+    static void R1_d(@Mutable({".i1", ".i1"}) @Release A x1) {
+    }
+
+    static void R1_e(@Immutable(".m2") @Release A x1) {
+        @Mutable Object m2 = x1.m2; // TODO: Report mutability exception here
+        ensureReadable(m2);
+        // :: error: permission.insufficient.shallow
+        ensureMutable(m2);
+    }
+
+    static void R1_f(@Immutable(".m2") @Release A x1) {
+        // :: error: permission.insufficient.shallow.borrowed
+        @Immutable Object i2 = x1.i2; // TODO: Report mutability exception here
+        // :: error: permission.insufficient.shallow
+        ensureReadable(i2);
     }
 }
