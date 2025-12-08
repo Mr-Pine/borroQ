aadd948d9351481db5a67556fdf09ad95d5d0a1a
MisterPine <git@mr-pine.de>
2025-12-08 15:39:00 +0100

    List test and stub start

---
+++ src/test/java/testcases/borroq/Lists.java
@@ -0,0 +1,33 @@
+package testcases.borroq;
+
+import de.mr_pine.borroq.qual.mutability.Immutable;
+import de.mr_pine.borroq.qual.mutability.Mutable;
+import de.mr_pine.borroq.qual.release.Release;
+
+import java.util.List;
+
+public interface Lists {
+    static void ensureMutable(@Mutable @Release Object x) {
+    }
+
+    static void ensureReadable(@Immutable @Release Object x) {
+    }
+
+    static void getElement(@Immutable @Release List<@Immutable Object> list, int i) {
+        @Immutable Object elem = list.get(i);
+        @Immutable Object elem2 = list.get(i + 1);
+
+        ensureReadable(elem);
+        ensureReadable(elem2);
+    }
+
+    static void innerMutOuterImmut(@Immutable @Release List<@Mutable Object> list, int i) {
+        @Mutable Object elem = list.get(i);
+        ensureMutable(elem);
+    }
+
+    static void innerImmutOuterMut(@Mutable @Release List<@Immutable Object> list, int i) {
+        @Mutable Object elem = list.get(i);
+        ensureMutable(elem);
+    }
+}
