e311a519040ac3edee828be6bb540dc6124d9630
MisterPine <git@mr-pine.de>
2026-01-18 10:14:59 +0100

    Add breaking counterexample for deleting borrows

---
+++ src/test/java/testcases/borroq/CounterexampleDeletingBorrows.java
@@ -0,0 +1,37 @@
+package testcases.borroq;
+
+import de.mr_pine.borroq.qual.mutability.Mutable;
+import de.mr_pine.borroq.qual.release.Borrow;
+
+public class CounterexampleDeletingBorrows {
+    static class Box {
+        Box(int value) {
+            this.value = value;
+        }
+        int value;
+    }
+    static class B {
+        B(@Mutable @Borrow Box x) {
+            this.x = x;
+        }
+        @Mutable Box x;
+    }
+    static class A {
+        A(@Mutable @Borrow B b) {
+            this.b = b;
+        }
+
+        @Mutable B b;
+    }
+
+    public static void main(String[] args) {
+        Box box = new Box(0);
+        B value = new B(box);
+        A a = new A(value);
+
+        B extracted = a.b;
+        Box extractedBox = extracted.x;
+        extractedBox.value = 1;
+        System.out.println(a.b.x.value);
+    }
+}
