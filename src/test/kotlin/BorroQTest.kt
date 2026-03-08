48b6e39a3b2df6bab848aefe7f400767f7282b5c
MisterPine <git@mr-pine.de>
2026-03-08 16:43:24 +0100

    Reenable all tests

--- src/test/kotlin/BorroQTest.kt
+++ src/test/kotlin/BorroQTest.kt
@@ -1,4 +1,3 @@
-
 import de.mr_pine.borroq.BorroQChecker
 import org.checkerframework.framework.test.CheckerFrameworkPerFileTest
 import org.checkerframework.framework.test.TestRootDirectory
@@ -17,16 +16,16 @@ class BorroQTest(testFile: File) : CheckerFrameworkPerFileTest(
     "-nowarn"
 ) {
     companion object {
-        /*@Suppress("unused")
-        @JvmStatic
-        @get:Parameterized.Parameters
-        val testDirs: Array<String?>
-            get() = arrayOf("borroq")*/
-
         @Suppress("unused")
         @JvmStatic
         @get:Parameterized.Parameters
+        val testDirs: Array<String?>
+            get() = arrayOf("borroq")
+
+        /*@Suppress("unused")
+        @JvmStatic
+        @get:Parameterized.Parameters
         val testFiles: List<File>
-            get() = listOf(File("src/test/java/testcases/borroq/Evaluation.java"))
+            get() = listOf(File("src/test/java/testcases/borroq/Evaluation.java"))*/
     }
 }
